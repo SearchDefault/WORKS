d0f73e284a1ad52376b42ed8e0672ce3f73403f2
Alex <43424882+SearchDefault@users.noreply.github.com>
2019-04-09 12:38:14 +0300

    Create z2_5.cpp

---
+++ inf_c++/z2_5.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+
+class BIN
+{
+		int x;
+	public:
+		BIN ();
+		void translit ();
+};
+
+BIN::BIN ()
+{
+	printf ( "Enter number: " );
+	scanf( "%i", &this->x );
+}
+
+void BIN::translit ()
+{
+	int num = this->x;
+	bool f = false;
+	
+	while ( num != 0 )
+	{
+		if ( (num & 1) && !f)
+			f = true;
+		if ( f )
+			printf ( "%i", num & 1 );
+		num = num >> 1;
+	}
+	
+}
+
+int main(int argc, char** argv) {
+	
+	BIN *a = new BIN;
+	
+	a -> translit ();
+	
+	delete a;
+	
+	return 0;
+}
