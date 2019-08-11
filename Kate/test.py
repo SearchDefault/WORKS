895f1b61f0b67488d361ee01a0ec5fc655a6a201
searchdefault <searchdefault@mail.ru>
2019-08-11 16:50:15 +0300

    new fiches

--- Kate/test.py
+++ Kate/test.py
@@ -1,49 +1,85 @@
 import speech_recognition as sr
 import pyaudio
 from espeak import espeak
+import pyttsx3 as tt
+import os
 import sys
 import webbrowser as wb
+import time
+
+opts = {
+	"names": ( "Kate", "Ann" ),
+	"cmds": {
+		"ctime": ( "time" ),
+		"radio": ( "radio" )
+	}
+}
 
 def talk ( words ):
-	print ( words )
-	espeak.synth ( words )
+    print ( "[log]" + words )
+    
+    if sys.platform == "linux" or sys.platform == "linux2":
+        espeak.set_voice ( "ru+f" )
+        espeak.synth ( words )
+
+        while espeak.is_playing ():
+            pass
+
+    elif sys.platform == "darwin":
+        os.system ( "say " + words )
+
+    elif sys.platform == "win32":
+        engine = tt.init ()
+        engine.say ( words )
+        engine.runAndWait ()
+        engine.stop()
 
 def command ():
     rec = sr.Recognizer ()
 
     with sr.Microphone () as sourse:
-        print ( "Say something:" )
+        print ( "[log] Скажи что-нибудь:" )
         rec.pause_threshold = 1
         rec.adjust_for_ambient_noise ( sourse, duration = 1 )
         audio = rec.listen ( sourse )
 
     try:
-        line = rec.recognize_google ( audio ).lower()
-        print ( "You say: " + line )
+        line = rec.recognize_google ( audio, language = "ru" ).lower()
+        print ( "Ты сказал: " + line )
 
     except sr.UnknownValueError:
-        talk ( "I'm not understand you" )
+        talk ( "Я тебя не понимаю!" )
         line = command ()
 
     return line
 
 def make ( line ):
-    if "open website" in line:
-        talk ( "One second" )
+    if "открой браузер" in line:
+        talk ( "Одну сикунду" )
         url = "https://google.com"
         wb.open ( url )
 
-    elif "adult films" in line:
-        talk ( "okey bad boy" )
-        url = "http://porno365.cc"
-        wb.open ( url )
+    elif "не спишь" in line or "ты тут" in line:
+        talk ( "Я не сплю сер!" )
+        talk ( "Жду ваших указаний!" )
 
-    elif "exit" in line or "stop" in line:
-        talk ( "I exit now" )
+    elif "пока" in line or "я ухожу" in line:
+        talk ( "Доброго вам дня сер!" )
         sys.exit ()
 
-    elif "name" in line:
-        talk ( "My name Kate!" )
+    elif "арбидол" in line:
+        talk ( "операция арбидол" )
+        url = "http://pornhub.com"
+        wb.open ( url )
+
+    elif "имя" in line or "тебя зовут" in line:
+        talk ( "Меня зовут Джарвис!" )
+    
+    elif "открой" and "страницу" in line:
+        string = line.split ( " " )
+        talk ( "Открываю вам" + string[ len( string ) - 1 ] + "страницу" )
+        url = "https://" + string [ len ( string ) - 1 ]
+        wb.open ( url )
 
 if __name__ == "__main__":
     while True:
